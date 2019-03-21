c5c255a1768ef3f1bfb444398fbecda22084272d
Ismaila Ba <isomaribaba@outlook.fr>
2019-03-21 13:34:48 +0000

    projet Heritage

--- Heritage/Controllers/UtilisateurController.cs
+++ Heritage/Controllers/UtilisateurController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Heritage.Models;
 
 namespace Heritage.Controllers
 {
     public class UtilisateurController : Controller
     {
         // GET: Utilisateur
-        public ActionResult utilisateur()
+        [HttpGet]
+        public ActionResult utilisateurId(string UtilisateurId)
         {
-            return View();
+            bool Status = false;
+            using (HeritageEntities dc = new HeritageEntities())
+            {
+                dc.Configuration.ValidateOnSaveEnabled = false;
+            }
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult utilisateur(Utilisateur utilisateur)
+        {
+
+            return View(utilisateur);
         }
     }
 }
